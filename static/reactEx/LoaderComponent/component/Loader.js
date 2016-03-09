e647b1f8cec1f0cd6d4733f1357dc1e0f0bb4698
akirsanov <Alexander.Kirsanov@reltio.com>
2016-03-09 18:45:19 +0300

    style has been applied.

--- static/reactEx/LoaderComponent/component/Loader.js
+++ static/reactEx/LoaderComponent/component/Loader.js
@@ -37,8 +37,31 @@ let Loader = class extends React.Component {
     }
     
     render() {
+        const spinnerStyle= {
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            marginLeft: '-60px',
+            marginTop: '-60px',
+            overflow: 'visible',
+            opacity: 0
+        };
+        const lineStyle = {
+            stroke: '#0083FF',
+            strokeWidth: 10,
+            strokeDasharray: [120, 37],
+            strokeLinecap: 'square',
+            strokeLinejoin: 'round',
+            fill: 'transparent'
+        };
+        const arrowStyle = {
+            fill: '#0083FF'
+        };
+        const tickStyle = {
+            opacity: 0
+        };
         return (
-            <svg className="spinner" version="1.1" xmlns="http://www.w3.org/2000/svg" width="120" height="120">
+            <svg style={spinnerStyle} version="1.1" xmlns="http://www.w3.org/2000/svg" width="120" height="120">
                 <defs>
                     <clipPath id="clipPath">
                         <circle cx="60" cy="60" r="55"/>
@@ -46,16 +69,16 @@ let Loader = class extends React.Component {
                 </defs>
                 <g className="background">
                     <g transform="translate(120, 0) scale(-1, 1)">
-                        <circle ref="line" className="line" cx="60" cy="60" r="50" clipPath="url(#clipPath)"/>
+                        <circle ref="line" style={lineStyle} cx="60" cy="60" r="50" clipPath="url(#clipPath)"/>
                         <g transform="translate(95, 45)">
-                            <path className="arrow" d="M1 16L14.2 0l13.2 15.8H1z"/>
+                            <path style={arrowStyle} d="M1 16L14.2 0l13.2 15.8H1z"/>
                         </g>
                         <g transform="translate(25, 75) rotate(180)">
-                            <path className="arrow" d="M1 16L14.2 0l13.2 15.8H1z"/>
+                            <path style={arrowStyle} d="M1 16L14.2 0l13.2 15.8H1z"/>
                         </g>
                     </g>
                 </g>
-                <g className="tick" transform="rotate(-45, 60, 60)">
+                <g style={tickStyle} transform="rotate(-45, 60, 60)">
                     <path fill="white" d="M17.977 35.553L0 16.276l5.112-4.768 17.727 19.01L52.31 0l4.93 4.76L22.83 40.39l-.316-.305-.168.156-2.98-3.194-1.465-1.416.074-.077z" transform="translate(28, 40) scale(1.15)" fill-rule="evenodd"/>
                 </g>
             </svg>
