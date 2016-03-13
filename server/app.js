6d924e333af19c6db6a1a02b4f6b3bfe67c9da93
Alexander.Kirsanov <Alexander.Kirsanov@reltio.com>
2016-03-13 16:32:07 +0300

    get content initial implementation

--- server/app.js
+++ server/app.js
@@ -6,27 +6,44 @@ const app = new Koa();
 const send = require('koa-send');
 const fs = require('fs');
 const url = require('url');
+const path = require('path');
 
 const isDependency = path => path.indexOf('node_modules') !== -1 || path.indexOf('package.json') !== -1;
 const isDirectory = file => fs.statSync(file).isDirectory();
 const isFiddle = dir => fs.existsSync(dir + '/index.js');
-const concatPath = path => file => path + '/'+ file;
+const concatPath = path => file => path + '/' + file;
 
 const list = (path) => {
     const children = fs.readdirSync(path).map(concatPath(path)).filter(isDirectory);
     const fiddles = children.filter(isFiddle);
     return [].concat(fiddles, children.map(list).reduce((xs, x) => xs.concat(x), []));
 };
-const getContent = (path) => {
-    const project = path ? path.split('=')[1] : '';
-    return {project:project};
+const fileList = (dir) => {
+    return fs.readdirSync(dir).reduce(function (list, file) {
+        var name = path.join(dir, file);
+        var isDir = fs.statSync(name).isDirectory();
+        return list.concat(isDir ? fileList(name) : [name]);
+    }, []);
+};
+const getContent = async(ctx, path) => {
+    const project = path ? path.split('=')[1].replace('/index', '/') : '';
+    return Promise.all(fileList(__dirname + '/../static/' + project).map((file)=> new Promise((resolve, reject) => {
+            fs.readFile(file, {encoding: 'utf-8'}, (err, data)=> {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve({file: file, data: data})
+                }
+            });
+        })
+    )).then(result => ctx.body = result);
 };
 const getFiddles = () => {
     const root = __dirname + '/../static';
     return list(root).map(l => l.substring(root.length + 1));
 };
 
-app.use(async (ctx, next) => {
+app.use(async(ctx, next) => {
     try {
         await next();
     } catch (err) {
@@ -35,15 +52,15 @@ app.use(async (ctx, next) => {
     }
 });
 
-app.use(async ctx => {
+app.use(async(ctx) => {
     if (isDependency(ctx.path)) {
         return await send(ctx, ctx.path, {root: __dirname + '/..'});
     } else if (ctx.path === '/' || ctx.path.startsWith('/list')) {
         return ctx.body = getFiddles()
             .map(name => `<li><a href="/index.html?folder=${name}">${name}</a>`)
             .join('');
-    } else if(ctx.path.startsWith('/source')) {
-        return ctx.body = getContent(url.parse(ctx.url).query);
+    } else if (ctx.path.startsWith('/source')) {
+        await getContent(ctx, url.parse(ctx.url).query);
     }
     await send(ctx, 'static' + ctx.path);
 });
