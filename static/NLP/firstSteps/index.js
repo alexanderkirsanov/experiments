6b11de7a0912c378311b45e9e8f500382fc5bd47
Alexander.Kirsanov <Alexander.Kirsanov@reltio.com>
2016-03-24 22:09:39 +0300

    initial npl implementation

--- static/NLP/firstSteps/index.js
+++ static/NLP/firstSteps/index.js
@@ -1,3 +1,17 @@
-var natural = require('natural'),
-    tokenizer = new natural.WordTokenizer();
-console.log(tokenizer.tokenize("your dog has fleas."));
+import bayes from 'bayes'
+const classifier = bayes();
+classifier.learn('open profile', 'profile');
+classifier.learn('show profile', 'profile');
+classifier.learn('show entity', 'profile');
+classifier.learn('show profile', 'profile');
+classifier.learn('show dashboard', 'dashboard');
+classifier.learn('open dashboard', 'dashboard');
+classifier.learn('search profile', 'search');
+classifier.learn('search hcp', 'search');
+classifier.learn('search location', 'search');
+
+console.log(classifier.categorize('search location near me'));
+console.log(classifier.categorize('search profile near me'));
+console.log(classifier.categorize('search hco'));
+console.log(classifier.categorize('open dashboard'));
+console.log(classifier.categorize('hi'));
