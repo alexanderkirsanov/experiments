a85089dccec94fab7ee5c33e254ce3ed864ce755
Alexander.Kirsanov <Alexander.Kirsanov@reltio.com>
2016-03-25 23:57:06 +0300

    improvements in npl

--- static/NLP/firstSteps/index.js
+++ static/NLP/firstSteps/index.js
@@ -2,23 +2,29 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import bayes from 'bayes'
 import ReactTranscriber from 'react-transcriber';
-let activeClassifier ;
-const profileProcessing = () => {
-    activeClassifier = profileClassifier();
-};
-const searchProcessing = () => {
+let activeClassifier;
+const resolve = (action) => {
+    return {
+        profileProcessing: () => {
+            activeClassifier = profileClassifier();
+        },
+        searchProcessing: () => {
 
-};
-const dashboard = () => {
+        },
+        dashboard: () => {
 
-};
+        },
+        first: () => console.log('first'),
+        second: () => console.log('second'),
+        third: () => console.log('third')
+    }[action];
+}
+
+
 const getTypes = () => [
-    {name: 'first', action: ()=>'test'},
-    {name: 'second', action: ()=>'second'},
-    {
-        name: 'third',
-        action: ()=>'third'
-    }];
+    {name: 'first', action: 'first'},
+    {name: 'second', action: 'second'},
+    {name: 'third', action: 'third'}];
 const profileClassifier = () => {
     const classifier = bayes();
     getTypes().forEach(type=> classifier.learn(type.name, type.action));
@@ -26,22 +32,23 @@ const profileClassifier = () => {
 };
 const basicClassifier = () => {
     const classifier = bayes();
-    classifier.learn('open profile', profileProcessing);
-    classifier.learn('show profile', profileProcessing);
-    classifier.learn('show entity', profileProcessing);
-    classifier.learn('show profile', profileProcessing);
-    classifier.learn('show dashboard', dashboard);
-    classifier.learn('open dashboard', dashboard);
-    classifier.learn('search profile', searchProcessing);
-    classifier.learn('search hcp', searchProcessing);
-    classifier.learn('search location', searchProcessing);
+    classifier.learn('open profile', 'profileProcessing');
+    classifier.learn('show profile', 'profileProcessing');
+    classifier.learn('show entity', 'profileProcessing');
+    classifier.learn('show profile', 'profileProcessing');
+    classifier.learn('show dashboard', 'dashboard');
+    classifier.learn('open dashboard', 'dashboard');
+    classifier.learn('search profile', 'searchProcessing');
+    classifier.learn('search hcp', 'searchProcessing');
+    classifier.learn('search location', 'searchProcessing');
     return classifier;
 };
+
 
 activeClassifier = basicClassifier();
 var test = (...arg) => {
     console.log(arg);
-    activeClassifier.categorize(arg[arg.length - 1].toLowerCase()).call(null);
+    resolve(activeClassifier.categorize(arg[arg.length - 1].toLowerCase())).call(null);
 };
 ReactDOM.render(<ReactTranscriber
     onTranscription={test.bind(this, 'standard')}/>, document.getElementById('playground'));
